e1eb9a5694aa661d091961667cb1a2de5c634596
Ben <heoroswow@gmail.com>
2020-03-17 16:33:06 -0500

    map implmented

--- stack.rb
+++ stack.rb
@@ -44,15 +44,19 @@ class Map
     end
 
     def set(key,value)
-
+        @array.push([key,value])
+        value
     end
 
     def get(key)
-
+        underlying_array.each { |pair| return pair[1] if pair[0] == key }
+        nil
     end
 
     def delete(key)
-
+        value = get(key)
+        underlying_array.reject! { |pair| pair[0] == key }
+        value
     end
 
     def show
